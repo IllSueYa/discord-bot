b21033af150679395c0b2b9d30f4b6d1e77bcfbb
IllSueYa <krazylizard901@gmail.com>
2026-08-28 22:47:02 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,5 @@ module.exports = {
       node_args: '-r dotenv/config',
       exp_backoff_restart_delay: 1000,
     },
-    {
-      name: 'helldivers2-support-bot',
-      script: './build/src/support.js',
-      node_args: '-r dotenv/config',
-    },
   ],
 };
